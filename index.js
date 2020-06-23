4e9b31797e5e1404ce381372561db92ff3e7ca69
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-23 12:22:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,5 +18,5 @@ BoardMember.prototype.releasePressStatement = function(){
   return 'You will see great things from Scuber.'
 }
 BoardMember.prototype.sayHi = function(){
-  return `Hi, my name is ${this.name}. `
+  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained`
 }
