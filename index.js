fa60f4ccae3d3e45135369234c9cf42dbe03a8b2
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-23 12:15:49 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+function BoardMember(name, homeState, training){
+  this.name = name;
+  this.homeState = homeState;
+  this.training = training;
+  veto = function(){
+    return 'No, I must disagree'
+  }
+  approve = function(){
+    'You can do that!'
+  }
+}
